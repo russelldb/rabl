1a571d24a5c20deec5e7ce9a881a3210df3a6ae1
Russell Brown <russell.brown@mac.com>
2017-07-19 12:05:20 +0100

    Flatten stats for CLI consumption

--- src/rabl_console.erl
+++ src/rabl_console.erl
@@ -9,6 +9,10 @@
 
 -compile(export_all).
 
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
 -spec show_config(list()) -> proplists:proplist().
 show_config([]) ->
     io:format("rabl env: ~p~n", [application:get_all_env(rabl)]).
@@ -51,5 +55,68 @@ status([]) ->
 -spec stats([]) -> ok.
 stats([]) ->
     {ok, Stats} = rabl_stat:get_stats(),
-    io:format("~p~n", [Stats]).
+    FlattendStats = flatten_stats(Stats),
+    io:format("~p~n", [FlattendStats]).
 
+%% @private take the proplist of stats, each a {name::atom(),
+%% stats::proplist()} tuple and create a flat set of stats of
+%% {name_stat, val}
+flatten_stats(Stats) ->
+    flatten_stats(Stats, []).
+
+flatten_stats([], Acc) ->
+    lists:flatten(Acc);
+flatten_stats([Stat | Stats], Acc) ->
+    flatten_stats(Stats, [flatten_stat(Stat) | Acc]).
+
+flatten_stat({Name, Stats}) ->
+    lists:foldl(fun({StatName, StatVal}, Acc) ->
+                        Flattened = flatten_stat(Name, StatName, StatVal),
+                        [Flattened | Acc]
+                end,
+                [],
+                Stats).
+
+-spec flatten_stat(list(), atom(), list() | number()) -> list().
+flatten_stat(Parent, Stat, Val) when is_number(Val) ->
+    Name = io_lib:format("~w_~w", [Parent, Stat]),
+    {list_to_atom(lists:flatten(Name)), Val};
+flatten_stat(Parent, Stat, Val) when is_list(Val) ->
+    Name = io_lib:format("~w_~w", [Parent, Stat]),
+    flatten_stat({list_to_atom(lists:flatten(Name)), Val}).
+
+-ifdef(TEST).
+
+flatten_stats_test() ->
+    Stats = [{queue_latency,[{min,0.0},
+                     {max,0.0},
+                     {arithmetic_mean,0.0},
+                     {geometric_mean,0.0},
+                     {harmonic_mean,0.0},
+                     {median,0.0},
+                     {variance,0.0},
+                     {standard_deviation,0.0},
+                     {skewness,0.0},
+                     {kurtosis,0.0},
+                     {percentile,[{50,0.0},
+                                  {75,0.0},
+                                  {90,0.0},
+                                  {95,0.0},
+                                  {99,0.0},
+                                  {999,0.0}]},
+                     {histogram,[{0,0}]},
+                     {n,0}]},
+         {consume,[{count,0},
+               {one,0},
+               {five,0},
+               {fifteen,0},
+               {day,0},
+               {mean,0.0},
+               {acceleration,[{one_to_five,0.0},
+                              {five_to_fifteen,0.0},
+                              {one_to_fifteen,0.0}]}]}],
+    Flat = flatten_stats(Stats),
+    [?assertMatch({Name, Val} when is_atom(Name) andalso is_number(Val),
+                                   FlatStat) || FlatStat <- Flat].
+
+-endif.
